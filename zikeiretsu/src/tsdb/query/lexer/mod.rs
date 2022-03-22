1cab748895e2a869112dec8f4015c554030f1e6b
tacogips <me@tacogips.me>
2022-03-22 17:42:36 +0900

    some

--- zikeiretsu/src/tsdb/query/lexer/mod.rs
+++ zikeiretsu/src/tsdb/query/lexer/mod.rs
@@ -59,6 +59,13 @@ impl BuildinMetrics {
     }
 }
 
+fn interpret_search_condition<'q>(
+    timezone: &FixedOffset,
+    where_clause: Option<&WhereClause<'q>>,
+) -> Result<DatapointSearchCondition> {
+    unimplemented!()
+}
+
 fn interpret_field_selector<'q>(
     column_index_map: Option<HashMap<&'q str, usize>>,
     select: Option<&SelectClause<'q>>,
@@ -156,59 +163,7 @@ pub fn interpret<'q>(parsed_query: ParsedQuery<'q>) -> Result<Query> {
 
     // select columns
     let field_selector = interpret_field_selector(column_index_map, parsed_query.select.as_ref())?;
+    let search_condition = interpret_search_condition(&timezone, parsed_query.r#where.as_ref())?;
 
-    //   match parsed_query.select {
-    //   None => return Err(LexerError::NoSelect),
-    //   Some(select) => {
-    //       if select
-    //           .select_columns
-    //           .iter()
-    //           .find(|each| *each == &Column::Asterick)
-    //           .is_some()
-    //       {
-    //           None
-    //       } else {
-    //           let mut field_selectors = Vec::<usize>::new();
-    //           match column_index_map {
-    //               None => {
-    //                   return Err(LexerError::NoColumnDef(format!(
-    //                       "columns :{}",
-    //                       select
-    //                           .select_columns
-    //                           .into_iter()
-    //                           .map(|e| e.to_string())
-    //                           .collect::<Vec<String>>()
-    //                           .join(",")
-    //                   )))
-    //               }
-    //               Some(column_index_map) => {
-    //                   for column in select.select_columns.iter() {
-    //                       if let Column::ColumnName(column_name) = column {
-    //                           match column_index_map.get(column_name.as_str()) {
-    //                               Some(column_idx) => field_selectors.push(*column_idx),
-    //                               None => {
-    //                                   return Err(LexerError::NoColumnDef(format!(
-    //                                       "{}",
-    //                                       column_name.as_str()
-    //                                   )))
-    //                               }
-    //                           }
-    //                       }
-    //                   }
-    //               }
-    //           }
-    //           None
-    //       }
-    //   }
-    //};
     unimplemented!()
 }
-
-//#[derive(Debug)]
-//pub struct ParsedQuery<'q> {
-//    pub with: Option<WithClause<'q>>,
-//    pub select: Option<SelectClause<'q>>,
-//    pub from: Option<FromClause<'q>>,
-//    pub r#where: Option<WhereClause<'q>>,
-//    pub order_or_limit: Option<OrderOrLimitClause<'q>>,
-//}

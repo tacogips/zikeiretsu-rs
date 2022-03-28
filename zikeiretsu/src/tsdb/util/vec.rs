465a845b4332c778c134c2694190275e30584a1d
tacogips <me@tacogips.me>
2022-03-28 15:32:58 +0900

    some

--- zikeiretsu/src/tsdb/util/vec.rs
+++ zikeiretsu/src/tsdb/util/vec.rs
@@ -5,6 +5,9 @@ type Result<T> = std::result::Result<T, VecOpeError>;
 pub enum VecOpeError {
     #[error("vec out of range: {0}")]
     OutOfRange(usize),
+
+    #[error("invalid Range: {0} {1}")]
+    InvalidRange(usize, usize),
 }
 
 pub fn remove_range<T>(datapoints: &mut Vec<T>, range: (usize, usize)) -> Result<()> {
@@ -49,22 +52,41 @@ pub fn trim_values<V>(
     values: &mut Vec<V>,
     retain_start_index: usize,
     cut_off_suffix_start_idx: usize,
-) -> Result<()> {
+) -> Result<()>
+where
+    V: std::fmt::Debug,
+{
+    if retain_start_index > cut_off_suffix_start_idx {
+        return Err(VecOpeError::InvalidRange(
+            retain_start_index,
+            cut_off_suffix_start_idx,
+        ));
+    }
+
+    if retain_start_index >= values.len() {
+        return Err(VecOpeError::OutOfRange(retain_start_index));
+    }
+
     let prefix_remove_range = if retain_start_index == 0 {
         None
     } else {
-        Some((0 as usize, retain_start_index))
+        Some((0 as usize, retain_start_index - 1))
     };
 
-    let suffix_remove_range = if cut_off_suffix_start_idx >= values.len() - 1 {
+    let suffix_remove_range = if cut_off_suffix_start_idx >= values.len() {
         None
     } else {
         if retain_start_index != 0 {
-            let prefix_trimmed_num = retain_start_index + 1;
+            let prefix_trimmed_num = retain_start_index;
             let len_after_prefix_trimmed = values.len() - prefix_trimmed_num;
+
+            println!(
+                "====== {} {} {}",
+                prefix_trimmed_num, cut_off_suffix_start_idx, len_after_prefix_trimmed
+            );
             Some((
                 cut_off_suffix_start_idx - prefix_trimmed_num,
-                len_after_prefix_trimmed,
+                len_after_prefix_trimmed - 1,
             ))
         } else {
             Some((cut_off_suffix_start_idx, values.len() - 1))
@@ -74,6 +96,8 @@ pub fn trim_values<V>(
     if let Some((start, end)) = prefix_remove_range {
         remove_range(values, (start, end))?;
     }
+
+    println!("==== {:?} ", values);
 
     if let Some((start, end)) = suffix_remove_range {
         remove_range(values, (start, end))?;
@@ -111,7 +135,7 @@ mod test {
             {1629745451_715066000, vec![300f64,36f64]}
         );
 
-        remove_range(&mut datapoints, (2, 3));
+        remove_range(&mut datapoints, (2, 3)).unwrap();
 
         let expected_datapoints = float_data_points!(
             {1629745451_715062000, vec![100f64,12f64]},
@@ -132,7 +156,7 @@ mod test {
             {1629745451_715066000, vec![300f64,36f64]}
         );
 
-        remove_range(&mut datapoints, (0, 4));
+        remove_range(&mut datapoints, (0, 4)).unwrap();
 
         let expected_datapoints = float_data_points!();
 
@@ -149,7 +173,7 @@ mod test {
             {1629745451_715066000, vec![300f64,36f64]}
         );
 
-        remove_range(&mut datapoints, (4, 4));
+        remove_range(&mut datapoints, (4, 4)).unwrap();
 
         let expected_datapoints = float_data_points!(
             {1629745451_715062000, vec![100f64,12f64]},
@@ -167,7 +191,7 @@ mod test {
             {1629745451_715062000, vec![100f64,12f64]}
         );
 
-        remove_range(&mut datapoints, (0, 0));
+        remove_range(&mut datapoints, (0, 0)).unwrap();
 
         let expected_datapoints = float_data_points!();
 
@@ -186,7 +210,7 @@ mod test {
             {1629745451_715067000, vec![300f64,36f64]}
         );
 
-        remove_range(&mut datapoints, (2, 4));
+        remove_range(&mut datapoints, (2, 4)).unwrap();
 
         let expected_datapoints = float_data_points!(
             {1629745451_715061000, vec![100f64,12f64]},
@@ -210,10 +234,62 @@ mod test {
             {1629745451_715067000, vec![300f64,36f64]}
         );
 
-        remove_range(&mut datapoints, (0, 6));
+        remove_range(&mut datapoints, (0, 6)).unwrap();
 
         let expected_datapoints = float_data_points!();
 
         assert_eq!(*datapoints, expected_datapoints);
+    }
+
+    #[tokio::test]
+    async fn trim_value_1() {
+        let mut data = vec![1, 2, 3, 4, 5, 6];
+
+        trim_values(&mut data, 0, 0).unwrap();
+
+        assert_eq!(Vec::<i32>::new(), data);
+    }
+
+    #[tokio::test]
+    async fn trim_value_2() {
+        let mut data = vec![1, 2, 3, 4, 5, 6];
+
+        trim_values(&mut data, 1, 3).unwrap();
+
+        assert_eq!(vec![2, 3], data);
+    }
+
+    #[tokio::test]
+    async fn trim_value_3() {
+        let mut data = vec![1, 2, 3, 4, 5, 6];
+
+        trim_values(&mut data, 0, 6).unwrap();
+
+        assert_eq!(vec![1, 2, 3, 4, 5, 6], data);
+    }
+
+    #[tokio::test]
+    async fn trim_value_4() {
+        let mut data = vec![1, 2, 3, 4, 5, 6];
+
+        trim_values(&mut data, 5, 6).unwrap();
+
+        assert_eq!(vec![6], data);
+    }
+
+    #[tokio::test]
+    async fn trim_value_5() {
+        let mut data = vec![1, 2, 3, 4, 5, 6];
+
+        trim_values(&mut data, 5, 5).unwrap();
+
+        assert_eq!(Vec::<i32>::new(), data);
+    }
+
+    #[tokio::test]
+    async fn trim_value_6() {
+        let mut data = vec![1, 2, 3, 4, 5, 6];
+
+        assert!(trim_values(&mut data, 6, 6).is_err());
     }
 }

a0459d010ae8e2dda63d24b4c892190acb4312e7
tacogips <me@tacogips.me>
2022-04-09 15:09:51 +0900

    some

--- zikeiretsu/src/tsdb/query/eval/describe_metrics.rs
+++ zikeiretsu/src/tsdb/query/eval/describe_metrics.rs
@@ -4,7 +4,7 @@ use crate::tsdb::engine::Engine;
 use crate::tsdb::query::lexer::{OutputCondition, OutputWriter};
 use crate::tsdb::query::DBContext;
 use crate::tsdb::{block_list, Metrics};
-use crate::tsdb::{DataSeriesRefs, StringDataSeriesRefs, StringSeriesRef};
+use crate::tsdb::{DataFrame, DataSeriesRefs, StringDataSeriesRefs, StringSeriesRef};
 use serde::Serialize;
 
 pub async fn execute_describe_metrics(
@@ -26,6 +26,9 @@ pub async fn execute_describe_metrics(
             )?,
         None => metricses,
     };
+    if metricses.is_empty() {
+        return Err(EvalError::MetricsNotFoundError("[empty]".to_string()));
+    }
 
     let metricses_strs = metricses
         .clone()
@@ -43,4 +46,75 @@ pub async fn execute_describe_metrics(
         output_with_condition!(output_condition, p_df);
     }
     Ok(metricses)
+}
+
+pub async fn load_metrics_describes(metrics: &[Metrics]) -> Result<(), EvalError> {
+    unimplemented!()
+}
+
+//pub async fn execute(describe_database_condition: DescribeDatabaseCondition) -> Result<()> {
+//    let metricses = Zikeiretsu::list_metrics(
+//        Some(describe_database_condition.db_dir.clone()),
+//        &describe_database_condition.setting,
+//    )
+//    .await?;
+//
+//    let mut describes = Vec::<DatabaseDescribe>::new();
+//    for metrics in metricses.into_iter() {
+//        let block_list = Zikeiretsu::block_list_data(
+//            &describe_database_condition.db_dir,
+//            &metrics,
+//            &describe_database_condition.setting,
+//        )
+//        .await?;
+//
+//        describes.push(DatabaseDescribe {
+//            metrics,
+//            block_list,
+//        });
+//    }
+//
+//    match describe_database_condition.output_setting.format {
+//        output::OutputFormat::Json => {
+//            let json_str = serde_json::to_string(&describes)
+//                .map_err(|e| output::OutputError::SerdeJsonError(e))?;
+//            describe_database_condition
+//                .output_setting
+//                .destination
+//                .write(vec![json_str])?
+//        }
+//        output::OutputFormat::Tsv => describe_database_condition
+//            .output_setting
+//            .destination
+//            .write(DatabaseDescribe::to_strs(describes))?,
+//    };
+//    Ok(())
+//}
+//
+
+struct MetrcisDescribe {
+    metrics: Metrics,
+    block_list: block_list::BlockList,
+}
+
+impl MetrcisDescribe {
+    fn datetime_range(&self) -> Vec<String> {
+        unimplemented!()
+        //let mut result = Vec::<String>::new();
+        //for each in describes {
+        //    result.push(format!(
+        //        "{metrics}\tupdated at:{updated_at}",
+        //        metrics = each.metrics,
+        //        updated_at = each.block_list.updated_timestamp_sec
+        //    ));
+        //    for ts in each.block_list.block_timestamps {
+        //        result.push(format!(
+        //            "\t{since}\t{until}",
+        //            since = ts.since_sec,
+        //            until = ts.until_sec
+        //        ));
+        //    }
+        //}
+        //result
+    }
 }

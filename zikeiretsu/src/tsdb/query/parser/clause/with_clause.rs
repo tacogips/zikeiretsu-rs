bfec192543a754d7f31601396524b15e31f3138a
tacogips <me@tacogips.me>
2022-04-23 10:43:39 +0900

    lint

--- zikeiretsu/src/tsdb/query/parser/clause/with_clause.rs
+++ zikeiretsu/src/tsdb/query/parser/clause/with_clause.rs
@@ -40,103 +40,92 @@ pub fn parse<'q>(pair: Pair<'q, Rule>) -> Result<WithClause<'q>> {
         def_sync_cloud: true,
     };
     for each in pair.into_inner() {
-        match each.as_rule() {
-            Rule::WITH_CLAUSE_DEFINES => {
-                for each_define in each.into_inner() {
-                    match each_define.as_rule() {
-                        Rule::DEFINE_COLUMNS => {
-                            for each_in_define_columns in each_define.into_inner() {
-                                if each_in_define_columns.as_rule() == Rule::COLUMNS {
-                                    let columns =
-                                        columns_parser::parse(each_in_define_columns, false)?;
+        if each.as_rule() == Rule::WITH_CLAUSE_DEFINES {
+            for each_define in each.into_inner() {
+                match each_define.as_rule() {
+                    Rule::DEFINE_COLUMNS => {
+                        for each_in_define_columns in each_define.into_inner() {
+                            if each_in_define_columns.as_rule() == Rule::COLUMNS {
+                                let columns = columns_parser::parse(each_in_define_columns, false)?;
 
-                                    with_clause.def_columns = Some(columns);
-                                }
+                                with_clause.def_columns = Some(columns);
                             }
                         }
+                    }
 
-                        Rule::DEFINE_DATABASE => {
-                            for each_in_define_database in each_define.into_inner() {
-                                if each_in_define_database.as_rule() == Rule::DB_NAME {
-                                    with_clause.def_database =
-                                        Some(each_in_define_database.as_str());
-                                }
+                    Rule::DEFINE_DATABASE => {
+                        for each_in_define_database in each_define.into_inner() {
+                            if each_in_define_database.as_rule() == Rule::DB_NAME {
+                                with_clause.def_database = Some(each_in_define_database.as_str());
                             }
                         }
+                    }
 
-                        Rule::DEFINE_TZ => {
-                            for each_in_define_tz in each_define.into_inner() {
-                                if each_in_define_tz.as_rule() == Rule::TIMEZONE_OFFSET_VAL {
-                                    let timezone =
-                                        timezone_parser::parse_timezone_offset(each_in_define_tz)?;
+                    Rule::DEFINE_TZ => {
+                        for each_in_define_tz in each_define.into_inner() {
+                            if each_in_define_tz.as_rule() == Rule::TIMEZONE_OFFSET_VAL {
+                                let timezone =
+                                    timezone_parser::parse_timezone_offset(each_in_define_tz)?;
 
-                                    with_clause.def_timezone = Some(timezone)
-                                } else if each_in_define_tz.as_rule() == Rule::TIMEZONE_NAME {
-                                    let timezone =
-                                        timezone_parser::parse_timezone_name(each_in_define_tz)?;
+                                with_clause.def_timezone = Some(timezone)
+                            } else if each_in_define_tz.as_rule() == Rule::TIMEZONE_NAME {
+                                let timezone =
+                                    timezone_parser::parse_timezone_name(each_in_define_tz)?;
 
-                                    with_clause.def_timezone = Some(timezone)
-                                }
+                                with_clause.def_timezone = Some(timezone)
                             }
                         }
+                    }
 
-                        Rule::DEFINE_FORMAT => {
-                            for each_in_define_tz in each_define.into_inner() {
-                                match each_in_define_tz.as_rule() {
-                                    Rule::KW_JSON => {
-                                        with_clause.def_output = Some(OutputFormat::Json)
-                                    }
-                                    Rule::KW_DATAFRAME => {
-                                        with_clause.def_output = Some(OutputFormat::DataFrame)
-                                    }
-
-                                    Rule::KW_PARQUET => {
-                                        with_clause.def_output = Some(OutputFormat::Parquet)
-                                    }
-                                    _ => { /* do nothing */ }
+                    Rule::DEFINE_FORMAT => {
+                        for each_in_define_tz in each_define.into_inner() {
+                            match each_in_define_tz.as_rule() {
+                                Rule::KW_JSON => with_clause.def_output = Some(OutputFormat::Json),
+                                Rule::KW_DATAFRAME => {
+                                    with_clause.def_output = Some(OutputFormat::DataFrame)
                                 }
+
+                                Rule::KW_PARQUET => {
+                                    with_clause.def_output = Some(OutputFormat::Parquet)
+                                }
+                                _ => { /* do nothing */ }
                             }
                         }
+                    }
 
-                        Rule::DEFINE_OUTPUT_FILE => {
-                            for each_in_output_file in each_define.into_inner() {
-                                match each_in_output_file.as_rule() {
-                                    Rule::FILE_PATH => {
-                                        with_clause.def_output_file_path =
-                                            Some(PathBuf::from(each_in_output_file.as_str()));
-                                    }
-                                    _ => { /* do nothing */ }
-                                }
+                    Rule::DEFINE_OUTPUT_FILE => {
+                        for each_in_output_file in each_define.into_inner() {
+                            if each_in_output_file.as_rule() == Rule::FILE_PATH {
+                                with_clause.def_output_file_path =
+                                    Some(PathBuf::from(each_in_output_file.as_str()));
                             }
                         }
+                    }
 
-                        Rule::DEFINE_CACHE => {
-                            for each_inner in each_define.into_inner() {
-                                if each_inner.as_rule() == Rule::BOOLEAN_VALUE {
-                                    with_clause.def_use_cache = parse_bool(each_inner)?;
-                                }
+                    Rule::DEFINE_CACHE => {
+                        for each_inner in each_define.into_inner() {
+                            if each_inner.as_rule() == Rule::BOOLEAN_VALUE {
+                                with_clause.def_use_cache = parse_bool(each_inner)?;
                             }
                         }
+                    }
 
-                        Rule::DEFINE_CLOUD => {
-                            for each_inner in each_define.into_inner() {
-                                if each_inner.as_rule() == Rule::BOOLEAN_VALUE {
-                                    with_clause.def_sync_cloud = parse_bool(each_inner)?;
-                                }
+                    Rule::DEFINE_CLOUD => {
+                        for each_inner in each_define.into_inner() {
+                            if each_inner.as_rule() == Rule::BOOLEAN_VALUE {
+                                with_clause.def_sync_cloud = parse_bool(each_inner)?;
                             }
                         }
+                    }
 
-                        _ => {
-                            return Err(ParserError::InvalidGrammer(format!(
-                                "invalid defines in with clause:{}",
-                                each_define
-                            )))
-                        }
+                    _ => {
+                        return Err(ParserError::InvalidGrammer(format!(
+                            "invalid defines in with clause:{}",
+                            each_define
+                        )))
                     }
                 }
             }
-
-            _ => { /* do nothing */ }
         }
     }
 

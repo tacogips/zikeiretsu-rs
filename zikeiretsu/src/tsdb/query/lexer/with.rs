0e7d35edbe127a591379f0190bedc89150caf95e
tacogips <me@tacogips.me>
2022-04-28 09:28:19 +0900

    change query

--- zikeiretsu/src/tsdb/query/lexer/with.rs
+++ zikeiretsu/src/tsdb/query/lexer/with.rs
@@ -93,7 +93,7 @@ pub(crate) fn interpret_with(with_clause: Option<WithClause<'_>>) -> LexerResult
         }
 
         // cloud setting
-        if with_clause.def_sync_cloud {
+        if with_clause.def_force_sync_cloud {
             with.cloud_setting = CloudStorageSetting::builder()
                 .force_update_block_list(true)
                 .build();
@@ -122,7 +122,7 @@ mod test {
             def_output: None,
             def_output_file_path: None,
             def_use_cache: true,
-            def_sync_cloud: true,
+            def_force_sync_cloud: true,
         };
 
         let result = interpret_with(Some(with_clause)).unwrap();

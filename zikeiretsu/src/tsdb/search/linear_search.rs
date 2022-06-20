d661bb988e79c21b8f65be9689c0c27bb55a73b0
tacogips <me@tacogips.me>
2022-06-20 17:12:02 +0900

    split search module

---
+++ zikeiretsu/src/tsdb/search/linear_search.rs
@@ -0,0 +1,240 @@
+#[derive(Eq, PartialEq)]
+pub enum LinearSearchDirection {
+    Asc,
+    Desc,
+}
+
+///search and return the index of element which match to condition.
+pub fn linear_search_last_index_which_match_rule<F, T>(
+    datas: &[T],
+    start_idx: usize,
+    cond: F,
+    search_direction: LinearSearchDirection,
+) -> Option<usize>
+where
+    F: Fn(&T) -> bool,
+{
+    if start_idx >= datas.len() {
+        return None;
+    }
+
+    let indices: Vec<usize> = if search_direction == LinearSearchDirection::Asc {
+        (start_idx..datas.len()).collect()
+    } else {
+        (0..(start_idx + 1)).rev().collect()
+    };
+    let mut latest_found_idx = None;
+
+    for idx in indices {
+        let curr_val = unsafe { datas.get_unchecked(idx) };
+        if cond(curr_val) {
+            latest_found_idx.replace(idx);
+        } else {
+            break;
+        }
+    }
+    latest_found_idx
+}
+
+pub fn linear_search_first_index_which_match_rule<F, T>(
+    datas: &[T],
+    start_idx: usize,
+    cond: F,
+    search_direction: LinearSearchDirection,
+) -> Option<usize>
+where
+    F: Fn(&T) -> bool,
+{
+    if start_idx >= datas.len() {
+        return None;
+    }
+
+    let indices: Vec<usize> = if search_direction == LinearSearchDirection::Asc {
+        (start_idx..datas.len()).collect()
+    } else {
+        (0..=start_idx).rev().collect()
+    };
+
+    for idx in indices {
+        let curr_val = unsafe { datas.get_unchecked(idx) };
+        if cond(curr_val) {
+            return Some(idx);
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::tsdb::*;
+    use std::cmp::Ordering;
+
+    macro_rules! empty_data_points {
+        ($($timestamp:expr),*) => {
+            vec![
+            $(DataPoint::new(TimestampNano::new($timestamp),vec![])),*
+            ]
+        };
+    }
+
+    #[test]
+    fn linear_search_same_timestamp_1() {
+        let datapoints: Vec<DataPoint> = empty_data_points!(10, 20, 20, 20, 30);
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                2,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(20),
+                LinearSearchDirection::Asc,
+            );
+            assert!(result.is_some());
+            let result = result.unwrap();
+
+            assert_eq!(result, 3);
+        }
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                2,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(20),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_some());
+            let result = result.unwrap();
+
+            assert_eq!(result, 1);
+        }
+    }
+
+    #[test]
+    fn linear_search_same_timestamp_2() {
+        let datapoints: Vec<DataPoint> = empty_data_points!(10, 20, 20, 20, 30);
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                2,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(19),
+                LinearSearchDirection::Asc,
+            );
+            assert!(result.is_none());
+        }
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                2,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(19),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_none());
+        }
+    }
+
+    #[test]
+    fn linear_search_same_timestamp_3() {
+        let datapoints: Vec<DataPoint> = empty_data_points!(10, 20, 20, 20, 30, 30);
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                datapoints.len() - 1,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Asc,
+            );
+            assert!(result.is_some());
+            assert_eq!(result.unwrap(), datapoints.len() - 1);
+        }
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                datapoints.len() - 1,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_some());
+            assert_eq!(result.unwrap(), datapoints.len() - 2);
+        }
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                datapoints.len(),
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Asc,
+            );
+            assert!(result.is_none());
+        }
+
+        {
+            let result = linear_search_last_index_which_match_rule(
+                &datapoints,
+                datapoints.len(),
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_none());
+        }
+    }
+
+    #[test]
+    fn linear_search_first_index_which_match_rule_timestamp_1() {
+        let datapoints: Vec<DataPoint> = empty_data_points!(10, 20, 20, 20, 30, 30);
+
+        {
+            let result = linear_search_first_index_which_match_rule(
+                &datapoints,
+                datapoints.len() - 1,
+                |datapoint| datapoint.timestamp_nano.cmp(&TimestampNano::new(30)) == Ordering::Less,
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_some());
+            assert_eq!(result.unwrap(), 3);
+        }
+
+        {
+            let result = linear_search_first_index_which_match_rule(
+                &datapoints,
+                datapoints.len() - 1,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_some());
+            assert_eq!(result.unwrap(), datapoints.len() - 1);
+        }
+
+        {
+            let result = linear_search_first_index_which_match_rule(
+                &datapoints,
+                datapoints.len(),
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Asc,
+            );
+            assert!(result.is_none());
+        }
+
+        {
+            let result = linear_search_first_index_which_match_rule(
+                &datapoints,
+                0,
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_none());
+        }
+
+        {
+            let result = linear_search_first_index_which_match_rule(
+                &datapoints,
+                datapoints.len(),
+                |datapoint| datapoint.timestamp_nano == TimestampNano::new(30),
+                LinearSearchDirection::Desc,
+            );
+            assert!(result.is_none());
+        }
+    }
+}

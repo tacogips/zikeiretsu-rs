96178e7058d8b9e696050a0f4a51b3e7fbbd3777
tacogips <me@tacogips.me>
2022-10-24 17:21:13 +0900

    zikeiretsu

--- zikeiretsu/src/tsdb/storage/wal/mod.rs
+++ zikeiretsu/src/tsdb/storage/wal/mod.rs
@@ -2,6 +2,7 @@ mod single_file_wal;
 
 use crate::tsdb::datapoint::DataPoint;
 use async_trait::async_trait;
+use bincode::Error as BincodeError;
 use std::io::{Error as IOError, Write};
 use std::path::Path;
 use thiserror::Error;
@@ -13,34 +14,35 @@ pub type Result<T> = std::result::Result<T, WalError>;
 pub enum WalError {
     #[error("io error: {0}")]
     IOError(#[from] IOError),
+    #[error("bincode error: {0}")]
+    BincodeError(#[from] BincodeError),
+
+    #[error("wal file open error: {0}")]
+    WalFileOpenError(String),
 }
 
 #[async_trait]
 pub trait WalWriter {
-    fn writer_channel(&self) -> Option<mpsc::Sender<DataPoint>>;
-    async fn write(&mut self, datapoint: &DataPoint) -> Result<()>;
-    fn load(&self) -> Result<Vec<DataPoint>>;
+    async fn write(&mut self, datapoint: &[DataPoint]) -> Result<()>;
+    async fn load(&self) -> Result<Vec<DataPoint>>;
     fn clean(&mut self) -> Result<()>;
-    fn exists(dir_path: &Path) -> bool;
+    fn exists(data_dir_path: &Path) -> bool;
 }
 
 pub struct EmptyWal;
 
 #[async_trait]
 impl WalWriter for EmptyWal {
-    fn writer_channel(&self) -> Option<mpsc::Sender<DataPoint>> {
-        None
-    }
-    async fn write(&mut self, datapoint: &DataPoint) -> Result<()> {
+    async fn write(&mut self, _datapoint: &[DataPoint]) -> Result<()> {
         Ok(())
     }
-    fn load(&self) -> Result<Vec<DataPoint>> {
+    async fn load(&self) -> Result<Vec<DataPoint>> {
         Ok(vec![])
     }
     fn clean(&mut self) -> Result<()> {
         Ok(())
     }
-    fn exists(dir_path: &Path) -> bool {
+    fn exists(data_dir_path: &Path) -> bool {
         false
     }
 }
--- zikeiretsu/src/tsdb/storage/wal/single_file_wal.rs
+++ zikeiretsu/src/tsdb/storage/wal/single_file_wal.rs
@@ -6,42 +6,81 @@
 use super::{Result, WalError, WalWriter};
 use crate::tsdb::datapoint::DataPoint;
 use async_trait::async_trait;
+use memmap2::MmapOptions;
 use std::fs::{File, OpenOptions};
-use std::io::{self, Seek};
+use std::io::{self, Read, Seek, Write};
 use std::path::{Path, PathBuf};
 use tokio::sync::mpsc;
+use tokio::task;
 
 const WAL_FILE_NAME: &str = "wal.dat";
 
 pub struct SingleFileWal {
     wal_file: File,
+}
 
-    sink: mpsc::Sender<DataPoint>,
-    source: mpsc::Receiver<DataPoint>,
+fn write_datapint_to_wal<W>(mut w: W, datapoint: &DataPoint) -> Result<()>
+where
+    W: Write,
+{
+    let serialized = bincode::serialize(&datapoint)?;
+    let data_size = serialized.len() as u64;
+
+    w.write_all(&data_size.to_be_bytes())?;
+    //TODO(tacodigs) if consequence data writing fails, should we delete the written data length
+    //also?
+    w.write_all(&serialized)?;
+    Ok(())
+}
+
+fn read_datapint_from_wal(datas: &[u8]) -> Result<Vec<DataPoint>> {
+    //let mut datapoints =Vec<DataPoint>::new();
+
+    //loop{
+    //    r.read(8)
+
+    //let serialized = bincode::deserialize(&datapoint)?;
+    //let data_size = serialized.len() as u64;
+
+    //w.write_all(&data_size.to_be_bytes())?;
+    ////TODO(tacodigs) if consequence data writing fails, should we delete the written data length
+    ////also?
+    //w.write_all(&serialized)?;
+
+    //}
+
+    //Ok(())
+    unimplemented!()
 }
 
 #[async_trait]
 impl WalWriter for SingleFileWal {
-    fn writer_channel(&self) -> Option<mpsc::Sender<DataPoint>> {
-        Some(self.sink.clone())
+    async fn write(&mut self, datapoints: &[DataPoint]) -> Result<()> {
+        for each_datapoint in datapoints {
+            write_datapint_to_wal(&mut self.wal_file, each_datapoint)?
+        }
+        Ok(())
     }
 
-    async fn write(&mut self, datapoint: &DataPoint) -> Result<()> {
-        unimplemented!()
+    async fn load(&self) -> Result<Vec<DataPoint>> {
+        let wal_data = unsafe {
+            MmapOptions::new()
+                .map(&self.wal_file)
+                .map_err(|e| WalError::WalFileOpenError(format!("{}", e)))?
+        };
+        let data = read_datapint_from_wal(&wal_data)?;
+        Ok(data)
     }
 
-    fn load(&self) -> Result<Vec<DataPoint>> {
-        unimplemented!()
-    }
     fn clean(&mut self) -> Result<()> {
         self.wal_file.set_len(0)?;
         Ok(())
     }
 
-    fn exists(dir_path: &Path) -> bool {
+    fn exists(data_dir_path: &Path) -> bool {
         let mut pb = PathBuf::new();
 
-        pb.push(dir_path);
+        pb.push(data_dir_path);
         pb.push(WAL_FILE_NAME);
 
         pb.exists()
@@ -49,17 +88,13 @@ impl WalWriter for SingleFileWal {
 }
 
 impl SingleFileWal {
-    pub fn new(dir_path: &Path, buf_size: usize) -> Result<Self> {
+    pub fn new(dir_path: &Path) -> Result<Self> {
         let mut pb = PathBuf::new();
         pb.push(dir_path);
         pb.push(WAL_FILE_NAME);
 
         let wal_file = OpenOptions::new().append(true).create(true).open(pb)?;
-        let (sink, source) = mpsc::channel(buf_size);
-        Ok(Self {
-            wal_file,
-            sink,
-            source,
-        })
+
+        Ok(Self { wal_file })
     }
 }
